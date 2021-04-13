fb7de5158fc54767f4d7499614db9c01c0909bda
Aleksander <aleksandergorecki80@gmail.com>
2021-04-14 00:43:53 +0100

    Append to html

--- app.js
+++ app.js
@@ -3,51 +3,72 @@
 // Mam konta w "X" różnych bankach
 
 class BankAcount {
-constructor() { 
-    this.seedCapital = parseFloat(15000);
-    this.capitalisationRate = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
-    this.depositIntrest = parseFloat((Math.random() * (0.05 - 0.01) + 0.01).toFixed(2));
-    this.transferCommision = parseFloat((Math.random() * (0.15 - 0.01) + 0.01).toFixed(2));
-}
+  constructor(depositName, seedCapital, capitalisationRate, depositIntrest, transferCommision) {
+    this.depositname = depositName;
+    this.seedCapital = seedCapital;
+    this.capitalisationRate = capitalisationRate;
+    this.depositIntrest = depositIntrest;
+    this.transferCommision = transferCommision
+  }
 
-getData = () => {
-    console.log(`Kapital: ${this.seedCapital} ${typeof this.seedCapital}`);
-    console.log(`Kapitalizacja co: ${this.capitalisationRate} ${typeof this.capitalisationRate}`);
-    console.log(`Procent: ${this.depositIntrest} ${typeof this.depositIntrest}`);
-    console.log(`Prowizja od przelewu: ${this.transferCommision} ${typeof this.transferCommision}`)
-}
+  getData = () => {
+   return `<div class="card" style="width: 18rem;">
+    <div class="card-header">
+     ${this.depositname}
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item"><b>Kapital</b>: ${this.seedCapital} ${typeof this.seedCapital}</li>
+      <li class="list-group-item">Kapitalizacja co: ${this.capitalisationRate} ${typeof this
+        .capitalisationRate}</li>
+      <li class="list-group-item">Prowizja od przelewu: ${this.transferCommision} ${typeof this
+        .transferCommision}</li>
+    </ul>
+  </div>`;
+  };
 
-getIntrests = () => {
+  getIntrests = () => {
     const result = this.seedCapital * this.depositIntrest;
-    const rounded = result.toFixed(2)
+    const rounded = result.toFixed(2);
     return parseFloat(rounded);
- }
- setCapital = (intrests) => {
-     this.seedCapital = parseFloat((this.seedCapital + intrests).toFixed(2));
- }
- getCapitalizationRate = () =>{
-     return this.capitalisationRate;
- }
+  };
+  setCapital = (intrests) => {
+    this.seedCapital = parseFloat((this.seedCapital + intrests).toFixed(2));
+  };
+  getCapitalizationRate = () => {
+    return this.capitalisationRate;
+  };
 }
 
 const bankAccounts = [];
-// for (i=0; i<5; i++){
-//     bankAccounts.push(new BankAcount());
-//     bankAccounts[i].getData();
-// }
+for (i = 0; i < 4; i++) {
+    const depositName = `Lokata nr <span>${i}</span>`
+    const seedCapital = parseFloat(15000);
+    const capitalisationRate = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
+    const depositIntrest = parseFloat(
+        (Math.random() * (0.15 - 0.01) + 0.01).toFixed(2)
+      );
+    const transferCommision = parseFloat(
+        (Math.random() * (0.15 - 0.01) + 0.01).toFixed(2)
+      );
 
-bankAccounts.push(new BankAcount());
-bankAccounts[0].getData()
-const rate = bankAccounts[0].getCapitalizationRate();
-console.log(rate)
+  bankAccounts.push(new BankAcount(depositName, seedCapital, capitalisationRate, depositIntrest, transferCommision));
+  const card = bankAccounts[i].getData();
+  console.log(card)
+  const container = document.getElementById('container');
+  container.insertAdjacentHTML('afterBegin', card);
+}
 
+// bankAccounts.push(new BankAcount());
+// bankAccounts[0].getData()
+// const rate = bankAccounts[0].getCapitalizationRate();
+// console.log(rate)
 
-setInterval(() => {
-const intrests = bankAccounts[0].getIntrests();
-console.log(intrests, 'intrests')
-bankAccounts[0].setCapital(intrests);
-bankAccounts[0].getData();
-}, rate*1000);
+// setInterval(() => {
+// const intrests = bankAccounts[0].getIntrests();
+// console.log(intrests, 'intrests')
+// bankAccounts[0].setCapital(intrests);
+// bankAccounts[0].getData();
+// }, rate*1000);
 
 // bankAccounts.push(new BankAcount());
 // bankAccounts[0].getData();
@@ -55,6 +76,5 @@ bankAccounts[0].getData();
 // console.log(typeof intrests, 'intrests')
 // bankAccounts[0].setCapital(intrests);
 // bankAccounts[0].getData();
-
 
 // console.log(bankAccounts)
